e6d7faf5e7fa6fd4f6bb8156e4b5ee486a2bfaa8
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2024-04-04 18:50:39 +0200

    validated BitLinear implementation

--- bitlinear.py
+++ bitlinear.py
@@ -30,15 +30,17 @@ class Ternarize(torch.autograd.Function):
         return grad_output
 
 class AbsMaxQuantize(torch.autograd.Function):
+    eps = 1e-5
+    b = 8
     @staticmethod
-    def forward(ctx, input, eps=1e-5, b=8):
-        Q_b = 2**(b-1)
+    def forward(ctx, input):
+        Q_b = 2**(AbsMaxQuantize.b-1)
         gamma = input.abs().max()
         quantized = torch.round(
             torch.clamp(
-                input*Q_b/(gamma+eps),
-                -Q_b+eps,
-                Q_b-eps,
+                input*Q_b/gamma,
+                -Q_b+AbsMaxQuantize.eps,
+                Q_b-AbsMaxQuantize.eps,
             ),
         )
         return quantized
@@ -81,12 +83,13 @@ class BitLinear(nn.Linear):
         self.allow_zero = allow_zero
 
     def forward(self, input):
-        normalized = torch.layer_norm(input, input.size()[1:])
-        binarized = Binarize.apply(self.weight)
-        output = F.linear(normalized, binarized, self.bias)
-        output = AbsMaxQuantize.apply(output)
-        output = output*output.abs().max()*self.weight.mean()/2**(self.activation_bits-1)
-        return output
+        normalized_activations = torch.layer_norm(input, input.size()[1:])
+        quantized_activations = AbsMaxQuantize.apply(normalized_activations)
+        quantized_weights = Ternarize.apply(self.weight) if self.allow_zero else Binarize.apply(self.weight)
+        quantized_outputs = F.linear(quantized_activations, quantized_weights, self.bias)
+        gamma = normalized_activations.abs().mean()
+        dequantized_output = quantized_outputs*self.weight.abs().mean()*gamma/2**(self.activation_bits-1)
+        return dequantized_output
 
 def replace_layer(model, old_class, new_class, **new_class_kwargs):
     for name, module in model.named_children():
@@ -98,4 +101,4 @@ def replace_layer(model, old_class, new_class, **new_class_kwargs):
             setattr(model, name, new_class(**kwargs))
             print(f"replaced layer {name} of class {old_class} with {new_class}")
         else:
-            replace_layer(module, old_class, new_class, **kwargs)
+            replace_layer(module, old_class, new_class, **new_class_kwargs)
