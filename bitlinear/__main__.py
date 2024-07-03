8c57382dd75b7511fe6afec52abba370908cf90a
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2024-07-03 15:00:03 +0200

    finished extraction and generation

--- bitlinear/__main__.py
+++ bitlinear/__main__.py
@@ -1,8 +1,12 @@
 import click
 
+from .commands.extract import _extract
+from .commands.generate import _generate
 from .commands.optimize import _optimize
 
 cli = click.CommandCollection(sources=[
+    _extract,
+    _generate,
     _optimize,
 ])
 
