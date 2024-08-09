6fbe6492b305136c17086628a24e6e9c04b8938e
Lukas Galke <git@lpag.de>
2024-08-10 00:19:12 +0200

    Set defaults for xor example

--- examples/xor/xor_example.py
+++ examples/xor/xor_example.py
@@ -12,8 +12,13 @@ import torch
 def round_clamp(input, range):
     return (input.round().clamp(range[0], range[1]) - input).detach() + input
 
+
 def scale(input, range, measure, keepdim, eps):
-    return max(abs(k) for k in range) / measure(input.detach(), keepdim=keepdim).clamp_(min=eps)
+    return max(abs(k) for k in range) / measure(input.detach(), keepdim=keepdim).clamp_(
+        min=eps
+    )
+
+
 ### END from bitlinear source
 
 
@@ -21,7 +26,6 @@ def quantize_weights(m: nn.Module):
     w_scale = scale(m.weight, m.weight_range, m.weight_measure, False, m.eps)
     w_quant = round_clamp(m.weight * w_scale, m.weight_range)
     return w_quant
-
 
 
 def main():
@@ -41,14 +45,15 @@ def main():
     bsz = 100
     break_on_first_solution = False
 
-    lr = 0.01
+    lr = 0.1
 
     n_hidden = 8  # BitLinear seems to need more than Linear
-    use_bitlinear = False  # change to false for basic MLP
+    use_bitlinear = True  # change to false for basic MLP
     weight_measure = "AbsMedian"  # AbsMean, AbsMedian
+    bitlinear_bias = False
 
     # Build toy dataset
-    x = (torch.rand(n, 4) < 0.5)
+    x = torch.rand(n, 4) < 0.5
     y = torch.logical_xor(x[:, 0], x[:, 1]).long()
     x = x.float()
 
@@ -60,11 +65,14 @@ def main():
 
     if use_bitlinear:
         # Basic MLP
-        model = nn.Sequential(BitLinear(4,n_hidden,weight_measure=weight_measure), nn.ReLU(), BitLinear(n_hidden, 2, weight_measure=weight_measure))
+        model = nn.Sequential(
+            BitLinear(4, n_hidden, weight_measure=weight_measure, bias=bitlinear_bias),
+            nn.ReLU(),
+            BitLinear(n_hidden, 2, weight_measure=weight_measure, bias=bitlinear_bias),
+        )
     else:
         # BitLinear MLP
-        model = nn.Sequential(nn.Linear(4,n_hidden), nn.ReLU(), nn.Linear(n_hidden, 2))
-
+        model = nn.Sequential(nn.Linear(4, n_hidden), nn.ReLU(), nn.Linear(n_hidden, 2))
 
     optimizer = AdamW(model.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
@@ -85,15 +93,14 @@ def main():
         # eval
         model.eval()
         y_hat = model(x_test)
-        acc = ((torch.argmax(y_hat, dim=-1) == y_test).sum().float() / y_test.size(0))
+        acc = (torch.argmax(y_hat, dim=-1) == y_test).sum().float() / y_test.size(0)
         print(f"Epoch {i} Accuracy: {acc.item()}")
         if break_on_first_solution and (acc - 1.0).abs() < 1e-8:
             break
 
-
     with torch.no_grad():
         # Check if inputs 2-3 are ignored
-        print("Model", model, sep='\n')
+        print("Model", model, sep="\n")
         print("Model weights:")
         for i, m in enumerate(model.modules()):
             print(m)
@@ -105,27 +112,48 @@ def main():
             print("BitLinear quantized weights", w_quant, sep="\n")
             print("w_quant.size()", w_quant.size())
 
-        print("-"*80)
+        print("-" * 80)
         print("Analyzing input2hidden layer...", model[0])
-        i2h_w = quantize_weights(model[0]) if isinstance(model[0], BitLinear) else model[0].weight
-        print("Input2hidden weights cols 0-1 (should be xor)", i2h_w[:, [0,1]], sep="\n")
-        print("Input2hidden weights cols 0-1 L1 norm:", torch.linalg.vector_norm(i2h_w[:, [0,1]], 1).item())
-        print("-"*80)
-        print("Input2hidden weights cols 2-3 (should be zero)", i2h_w[:, [2,3]], sep="\n")
-        print("Input2hidden weights cols 2-3 L1 norm:", torch.linalg.vector_norm(i2h_w[:, [2,3]], 1).item())
-        print("-"*80)
+        i2h_w = (
+            quantize_weights(model[0])
+            if isinstance(model[0], BitLinear)
+            else model[0].weight
+        )
+        print(
+            "Input2hidden weights cols 0-1 (should be xor)", i2h_w[:, [0, 1]], sep="\n"
+        )
+        print(
+            "Input2hidden weights cols 0-1 L1 norm:",
+            torch.linalg.vector_norm(i2h_w[:, [0, 1]], 1).item(),
+        )
+        print("-" * 80)
+        print(
+            "Input2hidden weights cols 2-3 (should be zero)", i2h_w[:, [2, 3]], sep="\n"
+        )
+        print(
+            "Input2hidden weights cols 2-3 L1 norm:",
+            torch.linalg.vector_norm(i2h_w[:, [2, 3]], 1).item(),
+        )
+        print("-" * 80)
         print("Analyzing input2hidden bias...", model[0].bias)
-        print("-"*80)
+        print("-" * 80)
 
-        print("-"*80)
+        print("-" * 80)
         print("Analyzing hidden2output layer...", model[2])
 
-        h2i_w = quantize_weights(model[-1]) if isinstance(model[-1], BitLinear) else model[-1].weight
+        h2i_w = (
+            quantize_weights(model[-1])
+            if isinstance(model[-1], BitLinear)
+            else model[-1].weight
+        )
         print("hidden2output weights", h2i_w, sep="\n")
-        print("hidden2output weights L1 norm:", torch.linalg.vector_norm(h2i_w, 1).item())
-        print("-"*80)
+        print(
+            "hidden2output weights L1 norm:", torch.linalg.vector_norm(h2i_w, 1).item()
+        )
+        print("-" * 80)
         print("Analyzing hidden2output bias...", model[2].bias)
-        print("-"*80)
+        print("-" * 80)
+
 
 if __name__ == "__main__":
     main()
