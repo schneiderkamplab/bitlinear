43536e97c6f93a7672a743eacd111f3976b42566
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2024-10-05 07:33:56 +0200

    maybe better like this?

--- bitlinear/bitlinear.py
+++ bitlinear/bitlinear.py
@@ -140,6 +140,8 @@ def bitlinearize(model, old_class=nn.Linear, new_class=BitLinear, replacements=[
 
 def set_lambda_(model, lambda_=1.0):
     assert 0.0 <= lambda_ <= 1.0
-    for module in model.modules():
-        if isinstance(module, BitLinear):
-            module.lambda_ = lambda_
+    for name, module in model.named_children():
+        if isinstance(module, BitLinear)
+            module.lambda_ = lambda_:
+        else:
+            set_lambda_(module, lambda_)
